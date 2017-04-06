c28c083cb6e6a260266f088d8a865a34e644585e
Jinseok Kim <jinsax@gmail.com>
2017-04-06 17:12:02 +0900

    Update plotly_visual.py

--- plotly_visual.py
+++ plotly_visual.py
@@ -5,7 +5,7 @@ import plotly.graph_objs as go
 import numpy as np
 
 tsne = TSNE(n_components = 3, learning_rate = 150, n_iter = 1000000000)
-X_tsne = tsne.fit_transform(visual)
+X_tsne = tsne.fit_transform(target)
 
 trace2 = go.Scatter3d(x = X_tsne[:countn*-1 - 1,0], y = X_tsne[:countn*-1 - 1,1], z = X_tsne[:countn*-1 - 1,2], text = lab[:countn*-1 - 1], textfont = {"size" : 10, "color": "#b3de69"}, mode = 'markers+text', marker = dict(size = 2, opacity = 0.8))
 trace0 = go.Scatter3d(x = X_tsne[-1,0], y = X_tsne[-1,1], z = X_tsne[-1,2], text = lab[-1], textfont = {"size" : 15, "color": "#bc80bd"}, mode = 'markers+text', marker = dict(size = 5, opacity = 0.9, color = 'rgba(255,0,0'))
