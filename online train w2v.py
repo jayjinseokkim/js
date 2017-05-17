c6511df9e3543c864f15caca3efc08b27e40fc48
Jinseok Kim <jinsax@gmail.com>
2017-05-17 16:46:01 +0900

    Update online train w2v.py

--- online train w2v.py
+++ online train w2v.py
@@ -8,6 +8,7 @@ import json
 sentences = []
 text = []
 start = time()
+i = 0
 
 path = 'C:/Users/jins_desktop/Downloads/702_20170405063215/702_webhose-2017-03_20170405063221'
 for filename in glob.glob(os.path.join(path, '*.json')):
@@ -16,11 +17,17 @@ for filename in glob.glob(os.path.join(path, '*.json')):
       json_line = json.loads(line)
       text = json_line['text']
       sentences.append(text)
+  i = i + 1
+  if (i % 10000 == 0):
+    print("Saved " + str(i) + " articles")
+  if (i == 300000):    ## as ceiling.
+    break
 print('took %.2f seconds to run.' %(time()-start))
 
 
+start = time()
 model = Word2Vec.load("wiki.en.word2vec.model")
 model.train(sentences)
 out_model = "wiki.en.word2vec.model_0517"
 model.save(out_model)
-
+print('took %.2f seconds to train.' %(time()-start))
